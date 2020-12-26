c0e0258636bc5488d4c2f122e4b071ffea9bdda7
Bassel Al Omari <baselomari@hotmail.com>
2020-12-27 00:14:27 +0200

    Enabled GUI for Game display

--- GUITest.py
+++ GUITest.py
@@ -1,4 +1,5 @@
 import pygame
+import Data Organization
 
 IMAGES = {}
 WIDTH = HEIGHT = 512
@@ -6,6 +7,7 @@ DIMENSION = 8
 SQ_SIZE = HEIGHT//DIMENSION
 MAX_FPS = 15
 
+board = 
 
 def load_images():
     for i in range(2):
@@ -13,11 +15,49 @@ def load_images():
             c = 'W'
         else:
             c = 'B'
-        IMAGES[f'{c}pawn'] = pygame.image.load(f"")
-        IMAGES[f'{c}king'] = pygame.image.load(f"C:\Users\romari\Desktop\Basel Code\Chess Piece Images\{c}_King")
-        IMAGES[f'{c}queen'] = pygame.image.load(f"C:\Users\romari\Desktop\Basel Code\Chess Piece Images\{c}_Queen")
-        IMAGES[f'{c}bishop'] = pygame.image.load(f"C:\Users\romari\Desktop\Basel Code\Chess Piece Images\{c}_Bishop")
-        IMAGES[f'{c}knight'] = pygame.image.load(f"C:\Users\romari\Desktop\Basel Code\Chess Piece Images\{c}_Knight")
-        IMAGES[f'{c}rook'] = pygame.image.load(f"C:\Users\romari\Desktop\Basel Code\Chess Piece Images\{c}_Rook")
+        IMAGES[f'{c}pawn'] = pygame.transform.scale(pygame.image.load(f"Chess Piece Images\\{c}_Pawn.png"), (SQ_SIZE,SQ_SIZE))
+        IMAGES[f'{c}king'] = pygame.transform.scale(pygame.image.load(f"Chess Piece Images\\{c}_King.png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[f'{c}queen'] = pygame.transform.scale(pygame.image.load(f"Chess Piece Images\\{c}_Queen.png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[f'{c}knight'] = pygame.transform.scale(pygame.image.load(f"Chess Piece Images\\{c}_Knight.png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[f'{c}bishop'] = pygame.transform.scale(pygame.image.load(f"Chess Piece Images\\{c}_Bishop.png"), (SQ_SIZE, SQ_SIZE))
+        IMAGES[f'{c}rook'] = pygame.transform.scale(pygame.image.load(f"Chess Piece Images\\{c}_Rook.png"), (SQ_SIZE, SQ_SIZE))
 
-load_images()
+def start_up():
+    pygame.init()
+    screen = pygame.display.set_mode((WIDTH,HEIGHT))
+    clock = pygame.time.Clock()
+    screen.fill(pygame.Color("White"))
+    load_images()
+
+    running = True
+
+    while running:
+        for e in pygame.event.get():
+            if e.type == pygame.QUIT:
+                running = False
+        
+
+        drawGameState(screen, board)
+        clock.tick(MAX_FPS)
+        pygame.display.flip()
+
+
+def drawGameState(screen, gs):
+    drawBoard(screen)
+    drawPieces(screen, gs.board)
+
+def drawBoard(screen):
+    colours = [pygame.Color("white"),pygame.Color("gray")]
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            colour = colours[((r + c) % 2)]
+            pygame.draw.rect(screen, colour, pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+            
+            
+
+def drawPieces(screen,board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c]
+            if piece != 0:
+                screen.blit(IMAGES[piece], pygame.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
